141971ce6dd3f0f2fba0818560fefb867b666427
Williams <williamsalcantara@gmail.com>
2024-11-25 11:18:55 -0300

    corrigindo erro ao criar quadro de aulas do eja 2º semestre

--- app/services/lesson_boards_service.rb
+++ app/services/lesson_boards_service.rb
@@ -30,16 +30,8 @@ class LessonBoardsService
     teacher_id = teacher_discipline_classroom.teacher.id
     year = teacher_discipline_classroom.classroom.year
 
-    linked = LessonsBoardLessonWeekday.includes(teacher_discipline_classroom: [:teacher, classroom: :unity])
-                                      .where(weekday: weekday)
-                                      .joins(lessons_board_lesson: :lessons_board,
-                                             teacher_discipline_classroom: [:teacher, classroom: :classrooms_grades])
-                                      .where(teachers: { id: teacher_id })
-                                      .where(classrooms: { year: year, period: period })
-                                      .where(lessons_board_lessons: { lesson_number: lesson_number })
-                                      .where.not(classrooms_grades: { classroom_id: classroom.to_i })
-                                      .first
-
+    linked = LessonsBoardLessonWeekday.by_classroom(classroom.to_i).first
+    
     return false if linked.nil?
 
     return false if end_period?(linked.teacher_discipline_classroom.classroom_id, classroom.to_i)
