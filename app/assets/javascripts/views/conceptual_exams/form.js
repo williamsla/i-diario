8a33aa0bb113b72d96cf0c2969380f17c3eb8ef9
Williams <williamsalcantara@gmail.com>
2024-11-26 01:01:32 -0300

    corrigindo bug ao carregar conceitos

--- app/assets/javascripts/views/conceptual_exams/form.js
+++ app/assets/javascripts/views/conceptual_exams/form.js
@@ -300,29 +300,51 @@ $(function () {
     flashMessages.error('Ocorreu um erro ao buscar as disciplinas da turma selecionada.');
   };
 
-  function loadSelect2ForConceptualExamValues() {
-    const lastElement = [...window.roundingTableValues].pop();
+  function loadSelect2ForConceptualExamValues(repeat=1) {
+    try {
+      const lastElement = [...window.roundingTableValues].pop();
 
-    _.each($('input.conceptual-exam-value-select2'), function (element) {
-      $(element).select2({
-        formatResult: function (el) {
-          return "<div class='select2-user-result'>" + el.name + "</div>";
-        },
-        formatSelection: function (el) {
-          return el.name;
-        },
-        data: $(element).data('elements')
-      });
-
-      if (!_.isEmpty(window.roundingTableValues)) {
+      _.each($('input.conceptual-exam-value-select2'), function (element) {
         $(element).select2({
-          data: window.roundingTableValues
+          formatResult: function (el) {
+            return "<div class='select2-user-result'>" + el.name + "</div>";
+          },
+          formatSelection: function (el) {
+            return el.name;
+          },
+          data: $(element).data('elements')
         });
-        
-        $(element).val(lastElement.id);
-        $(element).trigger('change');
+
+        if (!_.isEmpty(window.roundingTableValues)) {
+          $(element).select2({
+            data: window.roundingTableValues
+          });
+          
+          $(element).val(lastElement.id);
+          $(element).trigger('change');
+        }
+      });
+    } catch (error) {
+      console.error(error);
+      
+      if (repeat >= 5) {
+        return;
       }
-    });
+      sleep(2000);
+      loadSelect2ForConceptualExamValues(repeat + 1);
+    }
+    
+  }
+
+  function sleep(milliseconds) {
+    console.log('aguardando por '+ milliseconds + ' milisegundos');
+    
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > milliseconds){
+        break;
+      }
+    }
   }
 
   function removeDisciplines() {
