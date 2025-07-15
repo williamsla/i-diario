56ecb8866e0531ddcfe7ad34dc5fce5dc5340f79
williams <williamsalcantara@gmail.com>
2025-07-15 00:42:54 -0300

    filtrando apenas recuperacao da turma e disciplina selecionada

--- app/controllers/school_term_recovery_diary_records_controller.rb
+++ app/controllers/school_term_recovery_diary_records_controller.rb
@@ -291,7 +291,7 @@ class SchoolTermRecoveryDiaryRecordsController < ApplicationController
   def set_options_by_user
     @admin_or_teacher = current_user.current_role_is_admin_or_employee?
 
-    return fetch_linked_by_teacher unless @admin_or_teacher
+    # return fetch_linked_by_teacher if @admin_or_teacher
 
     @classrooms ||= [current_user_classroom]
     @disciplines ||= [current_user_discipline]
@@ -321,9 +321,9 @@ class SchoolTermRecoveryDiaryRecordsController < ApplicationController
       .ordered
       .distinct
 
-    unless @admin_or_teacher
-      @school_term_recovery_diary_records = @school_term_recovery_diary_records.by_teacher_id(current_teacher.id).distinct
-    end
+    # unless @admin_or_teacher
+    #   @school_term_recovery_diary_records = @school_term_recovery_diary_records.by_teacher_id(current_teacher.id).distinct
+    # end
   end
 
   def fetch_disciplines_by_classroom
