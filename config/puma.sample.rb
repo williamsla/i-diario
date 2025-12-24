4a1a41b7e2f527242538f8a49413f6f71c4674cf
williams <williamsalcantara@gmail.com>
2025-12-24 04:26:45 -0300

    puma

--- config/puma.sample.rb
+++ config/puma.sample.rb
@@ -1,9 +1,19 @@
 #!/usr/bin/env puma
 
 # Min and Max threads per worker
-threads 1, 1
+threads 2, 5
+workers 2
 
 preload_app!
+
+environment "production"
+
+bind "tcp://127.0.0.1:3000"
+
+pidfile "tmp/pids/puma.pid"
+
+stdout_redirect "log/puma.stdout.log", "log/puma.stderr.log", true
+worker_timeout 60
 
 on_worker_boot do
   ActiveSupport.on_load(:active_record) do
