6adca849c5b16d02779f9f66175db4c616038bee
williams <williamsalcantara@gmail.com>
2026-02-08 15:22:42 -0300

    criando evento por unidade

--- app/workers/school_calendar_event_batch_manager/event_creator_worker.rb
+++ app/workers/school_calendar_event_batch_manager/event_creator_worker.rb
@@ -46,6 +46,8 @@ module SchoolCalendarEventBatchManager
               event.periods = school_calendar_event_batch.periods
               event.legend = school_calendar_event_batch.legend
               event.show_in_frequency_record = school_calendar_event_batch.show_in_frequency_record
+              # Define coverage como by_unity para eventos em lote (padrão para eventos globais)
+              event.coverage = EventCoverageType::BY_UNITY
               
               if event.changed?
                 event.save!
