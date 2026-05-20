454610ac216b3afdd8e1ce745e2d83054be65628
williams <williamsalcantara@gmail.com>
2026-05-19 23:49:37 -0300

    sincronizando media da regra de avaliacao

--- db/migrate/20260519120000_add_average_for_promotion_to_exam_rules.rb
+++ db/migrate/20260519120000_add_average_for_promotion_to_exam_rules.rb
@@ -1,4 +1,4 @@
-class AddAverageForPromotionToExamRules < ActiveRecord::Migration[5.2]
+class AddAverageForPromotionToExamRules < ActiveRecord::Migration[5.0]
   def change
     add_column :exam_rules, :average_for_promotion, :decimal
   end
