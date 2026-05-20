2f8ace4ab95fe0e6a4e056b834155a4a2e33556c
williams <williamsalcantara@gmail.com>
2026-05-20 01:59:53 -0300

    descriptive

--- app/services/descriptive_exam_value.rb
+++ app/services/descriptive_exam_value.rb
@@ -6,7 +6,7 @@ module DescriptiveExamValue
 
     text = value.to_s
     text = text.gsub(/contenteditable\s*=\s*["'][^"']*["']/i, '')
-    text = ActionView::Base.full_sanitizer.strip_tags(text)
+    text = ActionView::Base.full_sanitizer.sanitize(text)
     text = CGI.unescapeHTML(text)
     text = text.gsub(/&nbsp;/i, ' ')
                .gsub(/\u00a0/, '')
